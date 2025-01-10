6c1087e86896791599c3083a5003d00b952656df
Amanda Gabriela <amandagabriela056@gmail.com>
2025-01-10 17:56:32 -0300

    criação da tela estática de gerar ticket

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { GenerateTicketComponent } from './features/generate-ticket/generate-ticket.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    component: GenerateTicketComponent,
+    path: 'gerar-ticket'
+  }
+];
---
+++ frontend/src/app/features/generate-ticket/generate-ticket.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generate-ticket',
+  standalone: true,
+  imports: [],
+  templateUrl: './generate-ticket.component.html',
+  styleUrl: './generate-ticket.component.scss'
+})
+export class GenerateTicketComponent {
+
+}
