1cb8e0b7cfc5ff8037fc52c533352d15fd30cd41
Amanda Gabriela <amandagabriela056@gmail.com>
2025-01-13 18:58:00 -0300

    realização do consumo do enpoint de listagem de tickets

--- frontend/src/app/features/guiche-panel/guiche-panel.component.ts
+++ frontend/src/app/features/guiche-panel/guiche-panel.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { TicketsService } from '../../core/services/tickets.service';
 
 @Component({
   selector: 'app-guiche-panel',
   standalone: true,
   imports: [],
   templateUrl: './guiche-panel.component.html',
-  styleUrl: './guiche-panel.component.scss'
+  styleUrl: './guiche-panel.component.scss',
 })
-export class GuichePanelComponent {
+export class GuichePanelComponent implements OnInit {
+tickets: any = [];
 
+  private ticketsService = inject(TicketsService);
+
+  ngOnInit(): void {
+    this.ticketsService.Get().subscribe({
+      next: (response) => {
+        console.log('Resposta da API:', response);
+        this.tickets = response;
+      },
+      error: (err) => {
+        console.error('Erro ao chamar a API:', err);
+      },
+    });
+  }
 }
