20616f338c2794be2c35fc6dad74d26ca962bd16
Amanda Gabriela <amandagabriela056@gmail.com>
2025-01-15 17:32:10 -0300

    add as rotas de perfis diferentes no header

--- frontend/src/app/core/components/header/header.component.ts
+++ frontend/src/app/core/components/header/header.component.ts
@@ -1,23 +1,38 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  @Input() title: string = ''; // Título principal
-  @Input() subTitle: string = ''; // Subtítulo
-  @Input() tabs: { label: string, value: string }[] = []; // Aba de navegação
+  @Input() title: string = '';
+  @Input() subTitle: string = '';
+  @Input() tabs: { label: string; value: string }[] = [];
+  @Input() selectedTab: string = '';
 
-  @Input() selectedTab: string = ''; // Aba atualmente selecionada
-  @Output() tabSelected = new EventEmitter<string>(); // Evento para informar a aba selecionada
+  @Output() tabSelected = new EventEmitter<string>();
 
+  private router = inject(Router);
+  profiles = [
+    { label: 'Paciente', value: 'pacientes' },
+    { label: 'Recepcionista', value: 'recepcionistas' },
+    { label: 'Médico', value: 'medicos' },
+    { label: 'Admin', value: 'admin' },
+  ];
+  selectedProfile: string = 'pacientes';
 
   selectTab(tabValue: string) {
     this.tabSelected.emit(tabValue);
+  }
+
+  onProfileChange(event: Event) {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    this.selectedProfile = selectedValue;
+    this.router.navigate(['/', selectedValue]);
+    console.log(`Navegando para: /${selectedValue}`);
   }
 }
